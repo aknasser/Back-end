98ffa3307af896f43574199cfccfbece05ce92f7
Nas La Menace <nassermassadimi@gmail.com>
2021-12-26 00:23:25 +0100

    collection Sorted in the descending order

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -4,7 +4,7 @@ module.exports = {
 
 
     retrieveProject : async (req, res, next) => {
-        const allProject = await Project.find({});
+        const allProject = await Project.find({}).sort({createdAt : -1});
         res.locals.toConvert = allProject;   // On cale allProject dans la variable locale "toConvertJSON". Cette variable est ensuite utilisée dans la middleware
         next(); 
      },
